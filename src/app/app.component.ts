eb571ec1f4279cfa1084fe867ee3e5aaf728854d
rri <raul.rincones@adorsys.com>
2022-12-06 12:28:26 +0100

    Session 5 component product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
+  products: Product[] = [
+    {
+      id: '67cb2af45',
+      name: 'Jim Morrisons book',
+      image: './assets/images/book1.jpeg',
+      price: 39,
+    },
+    {
+      id: '62cb1af15',
+      name: 'Beatles book',
+      image: './assets/images/book2.jpeg',
+      price: 29,
+    },
+    {
+      id: '32cb6af2',
+      name: 'Pink Floy book',
+      image: './assets/images/book3.jpeg',
+      price: 49,
+    },
+    {
+      id: '18cf6ab1',
+      name: 'Beastie Boys book',
+      image: './assets/images/book4.jpeg',
+      price: 29,
+    },
+    {
+      id: '15cb3ab0',
+      name: 'Metallica book',
+      image: './assets/images/book5.jpeg',
+      price: 48,
+    },
+  ];
 
   onLoaded(img: string) {
     console.log('loaded parent', img);
